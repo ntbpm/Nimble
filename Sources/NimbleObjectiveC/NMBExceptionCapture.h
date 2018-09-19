b3ee3d0616b7a8cd98ab3062504d7af2c3925427
ntbpm <ixyz169@163.com>
2018-09-19 20:44:38 +0800

    Update NMBExceptionCapture.h

--- Sources/NimbleObjectiveC/NMBExceptionCapture.h
+++ Sources/NimbleObjectiveC/NMBExceptionCapture.h
@@ -4,7 +4,7 @@
 @interface NMBExceptionCapture : NSObject
 
 - (nonnull instancetype)initWithHandler:(void(^ _Nullable)(NSException * _Nonnull))handler finally:(void(^ _Nullable)(void))finally;
-- (void)tryBlock:(__attribute__((noescape)) void(^ _Nonnull)(void))unsafeBlock NS_SWIFT_NAME(tryBlock(_:));
+- (void)tryBlock:(((noescape)) void(^ _Nonnull)(void))unsafeBlock NS_SWIFT_NAME(tryBlock(_:));
 
 @end
 
